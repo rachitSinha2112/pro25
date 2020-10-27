0829b9176d8fdebe342385782555aafb4a51ea10
rachitSinha2112 <71123839+rachitSinha2112@users.noreply.github.com>
2020-10-27 12:54:10 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -7,14 +7,14 @@ class Paper{
       density:1.2
    }
        this.image=loadImage("paper.png");
-       this.body= Matter.Bodies.circle(x,y,10,ball_options);
+       this.body= Matter.Bodies.circle(x,y,20,ball_options);
        World.add(world,this.body)  
  }
     display(){
       var pos=this.body.position;
       fill("white")
       imageMode(CENTER)    
-      image(this.image,pos.x,pos.y,10,10)
+      image(this.image,pos.x,pos.y,20,20)
    
     }
             
